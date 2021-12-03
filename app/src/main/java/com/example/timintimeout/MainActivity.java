d7a0fd9f4fa0e2cc42f00fe9693da1bacb9d3ed4
nicoderilo <nicoderilo@gmail.com>
2021-12-02 19:56:38 -0700

    timetimeout for rocs

---
+++ app/src/main/java/com/example/timintimeout/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.timintimeout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void getCurrentTime(View view) {
+        Calendar calendar = Calendar.getInstance();
+       // SimpleDateFormat mdformat = new SimpleDateFormat("HH:mm:ss");
+        SimpleDateFormat mdformat = new SimpleDateFormat("EEE-MMM-d \n HH:mm:ss");
+        String strDate = "Current Time : " + mdformat.format(calendar.getTime());
+        display(strDate);
+    }
+
+    private void display(String num) {
+        TextView textView = (TextView) findViewById(R.id.current_time_view);
+        textView.setText(num);
+    }
+}
