ea5d189d90c1ef2d463d3438a95d9c089bdd5e44
nicoderilo <nicoderilo@gmail.com>
2021-12-02 22:20:37 -0700

    display time, wit duration

--- app/src/main/java/com/example/timintimeout/MainActivity.java
+++ app/src/main/java/com/example/timintimeout/MainActivity.java
@@ -9,15 +9,18 @@ import android.widget.EditText;
 import android.widget.TextClock;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     EditText etStart, etEnd;
     Button btnDuration, btnTimeOut;
     TextClock textClock1, textClock2;
+    TextView current_time_view,tvDuration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,26 +29,30 @@ public class MainActivity extends AppCompatActivity {
         // time passes
         Instant end = Instant.now();
 
-
+        current_time_view = findViewById(R.id.current_time_view);
+        tvDuration = findViewById(R.id.tvDuration);
         etStart = findViewById(R.id.etStart);
         etEnd = findViewById(R.id.etEnd);
         btnDuration = findViewById(R.id.btnDuration);
         btnTimeOut = findViewById(R.id.btnTimeOut);
         textClock1 = findViewById(R.id.textClock1);
-        textClock2 = findViewById(R.id.textClock2);
+        //textClock2 = findViewById(R.id.textClock2);
 
 //        etStart.setText(start);
 
         btnTimeOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String startTime =  textClock1.getText().toString();
-                String endTime = textClock2.getText().toString();
-
+                String startTime =  current_time_view.getText().toString();
+                String endTime = current_time_view.getText().toString();
                 etStart.setText(startTime);
                 etEnd.setText(endTime);
-
 //            Duration timeElapsed = Duration.between(endTime, startTime);
+                //--------------------------------------
+             //String startTime =  current_time_view.getText().toString();
+             //String endTime = current_time_view.getText().toString();
+
+
 
             }
         });
@@ -56,6 +63,26 @@ public class MainActivity extends AppCompatActivity {
             //String startTime = etStart.getText().toString();
             //String endTime =etEnd.getText().toString();
            // Duration timeElapsed = Duration.between(etEnd.getText(), etStart.getText());
+                //Integer timeElapsed = Integer.parseInt(etEnd.getText())  - etStart.getText();
+                //Duration timeElapsed = Duration.between(etEnd.getText(), etStart.getText());
+
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+                try {
+                    Date time1 = simpleDateFormat.parse(etStart.getText().toString());
+                    Date time2 = simpleDateFormat.parse(etEnd.getText().toString());
+
+
+
+                    // Calculating the difference in milliseconds
+                    long differenceInMilliSeconds = Math.abs(time2.getTime() - time1.getTime());
+                    // Calculating the difference in Hours
+                    long differenceInHours = (differenceInMilliSeconds / (60 * 60 * 1000)) % 24;
+                    System.out.println(differenceInHours);
+                    System.out.println(differenceInMilliSeconds);
+                    tvDuration.setText(differenceInHours + "");
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
 
             }
         });
@@ -65,7 +92,8 @@ public class MainActivity extends AppCompatActivity {
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat mdformat = new SimpleDateFormat("HH:mm:ss");
         //SimpleDateFormat mdformat = new SimpleDateFormat("EEE-MMM-d \n HH:mm:ss");
-        String strDate = "Current Time : " + mdformat.format(calendar.getTime());
+        //String strDate = "Current Time : " + mdformat.format(calendar.getTime());
+        String strDate = mdformat.format(calendar.getTime());
         display(strDate);
 
 
